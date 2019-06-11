7bf4ab7cbd418307c341f0e2bcb328f8d8a7a465
desi Staunton <desi100@rogers.com>
2019-06-11 09:45:28 -0400

    amen

---
+++ README.py
@@ -0,0 +1,5 @@
+# hello-world
+print("first repository")
+print("my interest is programming")
+print("i am learning python3")
+print("you suck")
---
+++ hello-world.py
@@ -0,0 +1,6 @@
+# hello-world
+print("first repository")
+print("my interest is programming")
+print("i am learning python3")
+print("you suck")
+print("suck my big toe")
